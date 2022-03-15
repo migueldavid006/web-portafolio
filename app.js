86b10d69ba184eebb5dee51637b3458c2268c4b2
migueldavid006 <migueldavid006@hotmail.com>
2022-03-15 12:58:04 -0500

    portafolio I

---
+++ app.js
@@ -0,0 +1,10 @@
+function mostrarPremio(){
+    document.getElementById('premio').style.display = 'block';
+   document.getElementById('btnP').style.display = 'none';
+}
+
+function ocultarPremio(){
+    document.getElementById('premio').style.display = 'none';
+    document.getElementById('btnP').style.display = 'block';
+     
+}
